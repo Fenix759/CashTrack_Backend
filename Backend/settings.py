dc40a33a9d425d3c6a4637bfcfed34ade60105ad
Alan Ramirez <alanramirez8587@outlook.es>
2025-09-28 21:17:11 -0500

    Prueba OTP

--- Backend/settings.py
+++ Backend/settings.py
@@ -90,11 +90,11 @@ CORS_ALLOW_CREDENTIALS = True
 # Email
 # -------------------------
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.gmail.com"       # o el de tu proveedor
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")   # tu correo
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # tu contraseña o app password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # -------------------------
